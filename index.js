0e796de8805d784d15673b6e6a37aab9ffccb2bc
Yatin Nagpal <nnega4428@gmail.com>
2023-02-04 14:14:57 +1100

    setup a command file system to easily track files

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 // const { token } = require('./config.json');
 require('dotenv/config')
 
@@ -11,12 +11,32 @@ const client = new Client({
   ]
 });
 
+const prefix = '-';
+
+const path = require('node:path');
+const fs = require('fs')
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+  console.log(command.name, command);
+  client.commands.set(command.name, command);
+}
+
+
+
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
+
+
 client.on('messageCreate', msg => {
-  console.log(msg);
+
   if (msg.content.toLowerCase() === 'hi') {
     msg.reply('hello')
   }else if (msg.content === 'hell') {
@@ -30,9 +50,18 @@ client.on('messageCreate', msg => {
     default:
       break;
   }
-})
 
-const prefix = '-';
+  if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+
+  const args = msg.content.slice(prefix.length).split(/ +/)
+  const command = args.shift().toLowerCase()
+
+  if (command === 'ping') {
+    client.commands.get('ping').execute(msg, args)
+  }else if (command === 'google') {
+    msg.reply('https://www.youtube.com')
+  }
+})
 
 // Log in to Discord with your client's token
 client.login(process.env.TOKEN)
